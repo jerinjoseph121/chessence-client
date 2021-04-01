fa568b6f073abeeaa7198bc0088c9f6c73805023
Namrata Prasad <namratap2009@gmail.com>
2021-04-01 23:13:21 +0530

    modified spectators

--- com/chessence/gui/pages/components/Specs.java
+++ com/chessence/gui/pages/components/Specs.java
@@ -6,12 +6,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Specs extends JPanel implements ActionListener {
+public class Specs extends JPanel {
     private final JButton spectators = new RoundedButton("S P E C T A T O R S ➽",new Color(0xA05344), new Color(0x8A3D2E), 15);
     private Dimension size;
     JLayeredPane spec_panel = new JLayeredPane();
 
-    protected boolean hidden = true;
+    protected boolean hidden = false;
 
     public Specs(int W, int H) {
 
@@ -22,10 +22,13 @@ public class Specs extends JPanel implements ActionListener {
         //--------------------- OVERLAP PANEL FOR LIST OF SPECTATORS--------------------
 
         spec_panel.setPreferredSize(new Dimension(W,H));
-//        this.setLayout(null);
 
         spec_panel.setOpaque(true);
         spec_panel.setBackground(new Color(255,0,0,100));
+        spec_panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        RoundedButton x = new RoundedButton("B A C K ",new Color(0xA05344), new Color(0x8A3D2E), 15);
+        spec_panel.add(x);
+//        x.setBounds(0,0,W/2,50);
         this.add(spec_panel);
         spec_panel.setVisible(false);
 
@@ -34,18 +37,21 @@ public class Specs extends JPanel implements ActionListener {
         spectators.setFont(new Font("Geneva", Font.PLAIN,15));
         spectators.setForeground(new Color(0xFFAE56));
         spectators.addActionListener(new ActionListener() {
+
             public void actionPerformed(ActionEvent e) {
                 spec_panel.setVisible(true);
                 ((JButton) e.getSource()).setEnabled(false);
                 new Timer(1, new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
-                        spec_panel.setLocation(spec_panel.getX() - 1, 0);
-                        if (spec_panel.getX() - spec_panel.getWidth() == 0) {
+                        spec_panel.setLocation(spec_panel.getWidth(),0);
+                        if(spec_panel.getX()==spec_panel.getWidth()){
                             ((Timer) e.getSource()).stop();
-                            System.out.println("Timer stopped");
+                            System.out.println("dONE");
+                            hidden = true;
                         }
                     }
                 }).start();
+
             }
         });
 
@@ -63,12 +69,12 @@ public class Specs extends JPanel implements ActionListener {
 
             @Override
             public void mouseReleased(MouseEvent e) {
-
+                slide(true);
             }
 
             @Override
             public void mouseEntered(MouseEvent e) {
-                slide(true);
+
             }
 
             @Override
@@ -80,30 +86,22 @@ public class Specs extends JPanel implements ActionListener {
         super.setOpaque(false);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 
     private void slide(boolean show) {
-//        super.setPreferredSize(new Dimension(size.width,(int)size.height*8));
-//        final int w = spec_panel.getWidth();
-//        final int h = spec_panel.getHeight();
-//        final Point p1 = spec_panel.getLocation();
-//        final Point p2 = new Point(0, 0);
-//        int step = 1;
-//        if(show){
-//            while(p1.x>p2.x){
-//                p1.x -=step;
-//                spec_panel.setLocation(p1);
-//            }
-//        }else{
-//            p2.x=w;
-//            while(p1.x<p2.x){
-//                p1.x +=step;
-//                spec_panel.setLocation(p1);
-//            }
-//        }
+        super.setPreferredSize(new Dimension(size.width,(int)size.height*8));
+        new Timer(1, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                spec_panel.setLocation(spec_panel.getX() - 50, 0);
+
+                System.out.println(spec_panel.getX());
+                System.out.println(spec_panel.getWidth());
+                if (hidden && spec_panel.getX() <= 0) {
+                    spec_panel.setLocation(0, 0);
+                    ((Timer) e.getSource()).stop();
+                    System.out.println("Timer 2 stopped");
+                }
+            }
+        }).start();
     }
 
 
